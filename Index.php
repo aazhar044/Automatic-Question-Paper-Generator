b6ea24be79072667b59c6dc3bb4e39323596f241
Azhar Ali <34631042+azharali303@users.noreply.github.com>
2019-12-10 19:55:29 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,94 @@
+﻿<!DOCTYPE html>
+<?php 
+session_start();
+?>
+<html>
+<head>
+    <title>Question paper generator</title>
+    <link type="text/css" rel="stylesheet" href="StyleSheet1.css" />
+</head>
+<body>
+
+<?php
+ include ('header.php');?>
+</div>  
+ <div id="bottom">
+<br>
+<br>
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+<h3>UNIQUE FEATURES</h3>
+</marquee>
+<ol>
+<li type="circle">
+A Customised solution
+</li>
+<li type="circle">
+Generates a set of various types of questions.
+</li>
+<li type="circle">
+English and Mathematics
+</li>
+<li type="circle">
+Questions are based on learning objectives
+</li>
+<li type="circle">
+Can add user defined questions.
+</li>
+
+</ol>
+
+</div>
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+
+<h3>NEWS & EVENTS</h3>
+</marquee>
+<ol>
+<li type="circle">
+Date : 10-08-2019
+AQpgen.Com - Automatic Question Paper Generator website launched.
+</li>
+<li type="circle">
+Date : 15-09-2019 
+Complimentary membership for ABC customer schools.
+</li>
+</ol>
+</div>
+
+<div id="boxes">
+<marquee  behavior="alternate" width="100%" scrolldelay="200" >
+
+<h3>FEEDBACK CORNER</h3>
+</marquee>
+<u id="techr">Teachers Feedback</u>
+<ol>
+<li type="circle">
+A brilliant effort!! Would say congratulation JILIT! 
+Contains a vivid array of questions and keeps all the testing parameters in view while generating the question paper.
+Hats off!  
+</li>
+<li type="circle">
+Now we will have lots of time to utilize for other constructive work.</li>
+<li type="circle">
+It is something that we always required.</li>
+
+</ol>
+</div>
+
+<div id="boxes1">
+                                    
+<img height="300" width="1300" src="images.jpg"/>
+
+
+</div>
+
+
+ </div>
+
+<div id="footer">
+Content goes here</div>
+
+ <br/>
+</body>
+</html>
